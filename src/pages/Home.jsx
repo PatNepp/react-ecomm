20624bf048fcbc8872fe3daf3722ceffe0217c5b
Patrick Nepp <ptnepp@gmail.com>
2023-01-10 20:33:27 -0600

    announcement

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Announcement from '../components/Announcement'
 import Navbar from '../components/Navbar'
 
 const Home = () => {
   return (
     <div>
-        <Navbar name="Monkey" />
+        <Announcement 
+            announcement="Super Deal! Free Shipping on Orders Over $50" 
+        />
+        <Navbar />
     </div>
   )
 }
